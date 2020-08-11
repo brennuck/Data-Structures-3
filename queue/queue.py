06a195c57f9944020b288f6b425c5fbe42364f22
Brennon Nuckols <brennuck@gmail.com>
2020-08-11 13:58:31 -0600

    queue tests passing

--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,5 @@
+from singly_linked_list import LinkedList
+
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
@@ -16,13 +18,20 @@ Stretch: What if you could only use instances of your Stack class to implement t
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
     
-    def __len__(self):
-        pass
+    def __len__(self): # returns the number of elements in the queue
+        return self.size
 
-    def enqueue(self, value):
-        pass
+    def enqueue(self, value): # adds an element to the back of the queue
+        self.storage.add_to_tail(value)
+        self.size += 1
 
-    def dequeue(self):
-        pass
+    def dequeue(self): # removes and returns the element at the front of the queue
+        if self.size == 0: # if theres no element
+            return None # return none
+        else: # if there is an element or elements
+            element = self.storage.head.get_value() # assign element to the head value of the queue
+            self.storage.remove_head() # remove the head value of the queue
+            self.size += -1 # minus 1 on the size of the queue
+            return element # return the head element we removed
