66301978d0af396e3e2e57711d5964405fe2692d
Brennon Nuckols <brennuck@gmail.com>
2020-08-11 13:50:45 -0600

    stack tests passing

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,5 @@
+from singly_linked_list import LinkedList
+
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -13,13 +15,20 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
-    def __len__(self):
-        pass
+    def __len__(self): # returns the number of elements in the stack
+        return self.size
 
-    def push(self, value):
-        pass
+    def push(self, value): # adds an item to the top of the stack
+        self.storage.add_to_tail(value) # adds the given value to the top of the stack
+        self.size += 1 # plus 1 to the size of the stack
 
-    def pop(self):
-        pass
+    def pop(self): # removes and returns the element at the top of the stack
+        if self.size == 0: # if there is no element
+            return None # return none
+        else: # if there is elements
+            element = self.storage.tail.get_value() # gets the value of the tail
+            self.storage.remove_tail() # removes tail
+            self.size += -1 # lowers the size of the stack
+            return element # returns the tail that was removed
